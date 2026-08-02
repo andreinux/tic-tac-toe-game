eae98fdbf90ffac76c9bcd56cd0661697517f826
unknown <andreiacojido@gmail.com>
2026-08-02 17:23:11 +0800

    switch turn function

--- script.js
+++ script.js
@@ -12,6 +12,9 @@ tiles.forEach((tile)=> {
         console.log(gameboard.board);
 
         tile.textContent = "X";
+         
+        switchPlayer();
+        console.log(currentPlayer.name);
     })
 })
 
@@ -21,4 +24,24 @@ const gameboard = {
     board : ["", "", "", "", "", "", "", "", ""],
 };
 
+//player factory
+function createPlayer (name, marker){
+    return {
+        name,
+        marker
+    }
+}
 
+const player1 = createPlayer("Player 1", "X");
+const player2 = createPlayer("Player2" , "0");
+
+
+let currentPlayer =  player1;
+
+function switchPlayer (){
+    if (currentPlayer === player1){
+        currentPlayer = player2;
+    }else if (currentPlayer === player2){
+        currentPlayer = player1;
+    }
+}
