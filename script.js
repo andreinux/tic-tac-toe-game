16b8fe5383f39f9499cb6d758deb93a542bbd13c
unknown <andreiacojido@gmail.com>
2026-08-02 18:16:36 +0800

    check winner function

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ tiles.forEach((tile)=> {
 
     
         switchPlayer();
+        checkWinner();
         console.log(currentPlayer.name);
         console.log(gameboard.board);
     })
@@ -50,4 +51,43 @@ function switchPlayer (){
     }else if (currentPlayer === player2){
         currentPlayer = player1;
     }
+}
+
+
+    const winningPatterns = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+];
+
+//check winner function
+
+function checkWinner (){
+
+    for (const pattern of winningPatterns){
+    const a = pattern[0];
+    const b = pattern[1];
+    const c = pattern[2];
+    
+    if (
+        gameboard.board[a] !== "" &&
+        gameboard.board[a] === gameboard.board[b] &&
+        gameboard.board[b] === gameboard.board[c]
+    ){ if (currentPlayer === player1) {
+        alert("player1 won");
+    }else if ( currentPlayer === player2){
+        alert("Player2 won");
+    }
+    }
+
+
+    else {
+        console.log("continue playing");
+    }
+}
 }
