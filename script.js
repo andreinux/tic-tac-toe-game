e75f13f6216de1a3b7d6cc4cda80d47586173811
unknown <andreiacojido@gmail.com>
2026-08-02 17:10:43 +0800

    initial marking function

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("hello");
+
+//
+let tiles = document.querySelectorAll(".tile");
+
+tiles.forEach((tile)=> {
+    tile.addEventListener("click" , ()=> {
+        console.log(tile.dataset.id);
+
+        let targetIndex = tile.dataset.id;
+
+        gameboard.board[targetIndex] = "X";
+        console.log(gameboard.board);
+    })
+})
+
+//gameboard 
+
+const gameboard = {
+    board : ["", "", "", "", "", "", "", "", ""],
+};
+
+
