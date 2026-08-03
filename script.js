02e91946be2b922074a68eb2dca39acd2d1875a5
unknown <andreiacojido@gmail.com>
2026-08-03 11:14:17 +0800

    bug fix

--- script.js
+++ script.js
@@ -1,123 +1,6 @@
 
-//
-let gameOver = false;
 
-let player1Name = "Player 1";
-let player2Name = "Player 2";
-
-
-//signup
-
-let signupBtn = document.querySelector("#submit-btn");
-let signup = document.querySelector("#sign-up");
-let gameboardSection = document.querySelector("#gameboard");
-
-let player1Input = document.querySelector("#player1");
-let player2Input = document.querySelector("#player2");
-
-let matchName = document.querySelector("#match-name");
-
-
-
-signupBtn.addEventListener("click", (e)=> {
-    e.preventDefault();
-    
-     player1Name = player1Input.value.trim() || "Player 1";
-   player2Name = player2Input.value.trim() || "Player 2";
-
-   player1.name = player1Name;
-    player2.name = player2Name;
-    
-    signup.classList.add("hidden");
-    gameboardSection.classList.remove("hidden");
-
-    matchName.textContent = `${player1Name} vs ${player2Name}`
-})
-
-//game
-let tiles = document.querySelectorAll(".tile");
-
-tiles.forEach((tile) => {
-    tile.addEventListener("click", () => {
-        if (gameOver ===  true) return;
-
-        const targetIndex = tile.dataset.id;
-
-        if (gameboard.board[targetIndex] !== "") return;
-
-        gameboard.board[targetIndex] = currentPlayer.marker;
-        tile.textContent = currentPlayer.marker;
-
-        if (checkWinner()) {
-            return; 
-        }
-
-        if (!gameboard.board.includes("")) {
-            matchWinner.textContent = "It's a Tie!";
-            return;
-        }
-
-        switchPlayer();
-    });
-});
-
-//gameboard 
-
-
-//player factory
-function createPlayer (name, marker){
-    return {
-        name,
-        marker
-    }
-}
-
-const player1 = createPlayer(player1Name, "X");
-const player2 = createPlayer(player2Name , "O");
-
-
-let currentPlayer =  player1;
-
-function switchPlayer (){
-    if (currentPlayer === player1){
-        currentPlayer = player2;
-    }else if (currentPlayer === player2){
-        currentPlayer = player1;
-    }
-}
-
-
-//check winner function
-let matchWinner = document.querySelector("#match-winner");
-function checkWinner() {
-    for (const pattern of winningPatterns) {
-
-        const a = pattern[0];
-        const b = pattern[1];
-        const c = pattern[2];
-
-        if (
-            gameboard.board[a] !== "" &&
-            gameboard.board[a] === gameboard.board[b] &&
-            gameboard.board[b] === gameboard.board[c]
-        ) {
-            matchWinner.textContent = `${currentPlayer.name} wins!`;
-            
-            gameOver =  true;
-            return true;
-        }
-    }
-
-    return false;
-}
-
-let restartBtn = document.querySelector("#restartBtn");
-
-restartBtn.addEventListener("click", ()=> {
-    restart();
-})
-
-
+//GAMEBOARD MODULE
 const GameBoard = (()=> {
     
     
@@ -148,12 +31,139 @@ return {
 })();
 
 
+//GAMECONTROLLER MODULE
+const GameController = (()=> {
+
+    
+
+let player1Name = "Player 1";
+let player2Name = "Player 2";
+
+function setPlayerNames(name1, name2) {
+    player1.name = name1;
+    player2.name = name2;
+}
+
+    
+const player1 = createPlayer(player1Name, "X");
+const player2 = createPlayer(player2Name , "O");
+
+
+let currentPlayer =  player1;
+let gameOver = false;
+
+
+function switchPlayer (){
+    if (currentPlayer === player1){
+        currentPlayer = player2;
+    }else if (currentPlayer === player2){
+        currentPlayer = player1;
+    }
+}
+
+function checkWinner() {
+    for (const pattern of GameBoard.winningPatterns) {
+
+        const a = pattern[0];
+        const b = pattern[1];
+        const c = pattern[2];
+
+        if (
+            GameBoard.board[a] !== "" &&
+            GameBoard.board[a] === GameBoard.board[b] &&
+            GameBoard.board[b] === GameBoard.board[c]
+        ) {
+            matchWinner.textContent = `${currentPlayer.name} wins!`;
+            return currentPlayer;
+            gameOver =  true;
+            return true;
+        }
+    }
+
+    return false;
+}
+
+
 function reset (){
-    currentPlayer = player1;
+    GameBoard.board.fill("");
+}
+
+function getCurrentPlayer() {
+    return currentPlayer;
+}
+
+return {
+    currentPlayer, gameOver, player1Name, player2Name, switchPlayer, getCurrentPlayer,
+    setPlayerNames, checkWinner, reset
+}
+
+})();
+
+
+//player factory
+function createPlayer (name, marker){
+    return {
+        name,
+        marker
+    }
+}
+
+
+const DisplayController = (()=> {
+    
+let signupBtn = document.querySelector("#submit-btn");
+let signup = document.querySelector("#sign-up");
+let gameboardSection = document.querySelector("#gameboard");
+let player1Input = document.querySelector("#player1");
+let player2Input = document.querySelector("#player2");
+let matchName = document.querySelector("#match-name");
+let tiles = document.querySelectorAll(".tile");
+let matchWinner = document.querySelector("#match-winner");
+let restartBtn = document.querySelector("#restartBtn");
+
+        
+signupBtn.addEventListener("click", (e)=> {
+    e.preventDefault();
+    
+     const player1Name = player1Input.value.trim() || "Player 1";
+const player2Name = player2Input.value.trim() || "Player 2";
+
+GameController.setPlayerNames(player1Name, player2Name);
+    
+    signup.classList.add("hidden");
+    gameboardSection.classList.remove("hidden");
+
+    matchName.textContent = `${player1Name} vs ${player2Name}`
+})
+
+restartBtn.addEventListener("click", ()=> {
     tiles.forEach((tile)=> {
         tile.textContent = "";
+        GameController.reset();
     })
+})
 
-    matchWinner.textContent = "";
-    gameOver = false;
+
+    
+tiles.forEach((tile) => {
+    tile.addEventListener("click", () => {
+        
+        const targetIndex = tile.dataset.id;
+        if (GameBoard.board[targetIndex] !== "") return;
+
+        GameBoard.board[targetIndex] = GameController.getCurrentPlayer().marker;
+        tile.textContent = GameController.getCurrentPlayer().marker;
+
+        GameController.switchPlayer();
+
+    });
+});
+
+const winner = GameController.checkWinner();
+
+if (winner) {
+    matchWinner.textContent = `${winner.name} wins!`;
 }
+
+})();
+
